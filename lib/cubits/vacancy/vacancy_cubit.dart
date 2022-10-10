35a5e00575950c9fd51d8d8e61ea674909fa9bc2
Shokhruzmukhtarov <shoxruzchik880@gmail.com>
2022-10-10 14:30:58 +0500

    just did the second page

--- lib/cubits/vacancy/vacancy_cubit.dart
+++ lib/cubits/vacancy/vacancy_cubit.dart
@@ -1,5 +1,6 @@
+import 'dart:async';
 import 'package:bloc/bloc.dart';
-import 'package:ish_top/data/repositories/announcement/announcement_repository.dart';
+import 'package:ish_top/data/models/vacancy/vacancy_model.dart';
 import 'package:ish_top/data/repositories/helper/helper_repository.dart';
 import 'package:ish_top/data/repositories/vacancy/vacancy_repository.dart';
 import 'package:meta/meta.dart';
@@ -13,5 +14,37 @@ class VacancyCubit extends Cubit<VacancyState> {
   }) : super(VacancyInitial());
 
   final VacancyRepository vacancyRepository;
+
+  late StreamSubscription _subscription;
   final HelperRepository helperRepository;
+
+  Future<void> listenToVacancies() async {
+    emit(GetVacancyProgress());
+    _subscription = vacancyRepository.getVacancies().listen(
+          (items) {
+        emit(GetVacancyInSuccess(vacancies: items));
+      },
+      onError: (error) {
+        emit(GetVacancyInFailure(errorText: error.toString()));
+      },
+    );
+  }
+
+  Future<void> listenToVacanciesById({required String categoryId}) async {
+    emit(GetVacancyProgress());
+    _subscription = vacancyRepository.getVacanciesByCategory(categoryId: categoryId).listen(
+          (items) {
+        emit(GetVacancyInSuccess(vacancies: items));
+      },
+      onError: (error) {
+        emit(GetVacancyInFailure(errorText: error.toString()));
+      },
+    );
+  }
+
+  @override
+  Future<void> close() {
+    _subscription.cancel();
+    return super.close();
+  }
 }
