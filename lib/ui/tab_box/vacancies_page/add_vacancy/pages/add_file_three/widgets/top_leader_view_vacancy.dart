93a65791f4dfa7b0f188ce514873e62cae40d027
Sodiqjon95 <sodiqdev95@gmil.com>
2022-10-12 20:45:26 +0500

    add top_level_view_vacancy and thre page

---
+++ lib/ui/tab_box/vacancies_page/add_vacancy/pages/add_file_three/widgets/top_leader_view_vacancy.dart
@@ -0,0 +1,54 @@
+import 'package:flutter/material.dart';
+import 'package:ish_top/utils/style.dart';
+
+class TopLeaderViewVacancy extends StatelessWidget {
+  const TopLeaderViewVacancy({Key? key, required this.currentPage}) : super(key: key);
+
+  final int currentPage;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: 50,
+      margin: const EdgeInsets.symmetric(horizontal: 16),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          getRoundedWidget(currentPage >= 1, "1"),
+          Expanded(
+            child: Container(
+              height: 1,
+              color: Colors.grey,
+            ),
+          ),
+          getRoundedWidget(currentPage >= 2, "2"),
+          Expanded(
+            child: Container(
+              height: 1,
+              color: Colors.grey,
+            ),
+          ),
+          getRoundedWidget(currentPage >= 3, "3"),
+
+        ],
+      ),
+    );
+  }
+
+  Widget getRoundedWidget(bool isActive, String text) {
+    return Container(
+      height: 30,
+      width: 30,
+      decoration: BoxDecoration(
+        color: isActive ? Color(0xff356899) : Color(0xff95969d),
+        shape: BoxShape.circle,
+      ),
+      child: Center(
+        child: Text(
+          text,
+          style: MyTextStyle.sfProBold.copyWith(color: Colors.white),
+        ),
+      ),
+    );
+  }
+}
