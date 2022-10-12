5c988d7e7ba4a8a2ca38ce2acd9339e6ab21d3a6
Saidislom99 <saidislomnajimov@gmail.com>
2022-10-12 14:53:41 +0500

    completed

--- lib/ui/tab_box/vacancies_page/widgets/vacancy_item.dart
+++ lib/ui/tab_box/vacancies_page/widgets/vacancy_item.dart
@@ -5,18 +5,20 @@ import 'package:ish_top/utils/icon.dart';
 import 'package:ish_top/utils/style.dart';
 
 import '../../../../utils/color.dart';
+
 class VacancyItem extends StatelessWidget {
-  const VacancyItem({Key? key,
-    required this.onTap,
-  required this.brandImage,
-     required this.companyName,
-    required this.fromWhere,
-    required this.jobTitle,
-    required this.offeredSalary,
-    required this.requiredLevel,
-    required this.jobType,
-    required this.recruiterPhone
-  }) : super(key: key);
+  const VacancyItem(
+      {Key? key,
+      required this.onTap,
+      required this.brandImage,
+      required this.companyName,
+      required this.fromWhere,
+      required this.jobTitle,
+      required this.offeredSalary,
+      required this.requiredLevel,
+      required this.jobType,
+      required this.recruiterPhone})
+      : super(key: key);
   final String brandImage;
   final String companyName;
   final String jobTitle;
@@ -27,47 +29,60 @@ class VacancyItem extends StatelessWidget {
   final String recruiterPhone;
   final VoidCallback onTap;
 
-
-
-
-
   @override
   Widget build(BuildContext context) {
-    return  InkWell(
-      onTap:onTap ,
+    return InkWell(
+      onTap: onTap,
       child: Container(
         height: 200,
         margin: const EdgeInsets.symmetric(vertical: 12),
-        padding: const  EdgeInsets.symmetric(horizontal: 20,vertical: 15),
+        padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 15),
         decoration: BoxDecoration(
             borderRadius: BorderRadius.circular(16),
             color: MyColors.C_5386E4,
             boxShadow: [
               BoxShadow(
                 color: Colors.grey.withOpacity(0.3),
-                offset: const Offset(1,3),
+                offset: const Offset(1, 3),
                 blurRadius: 3,
                 spreadRadius: 5,
               )
-            ]
-        ),
+            ]),
         child: Column(
           mainAxisAlignment: MainAxisAlignment.spaceAround,
           children: [
             Row(
               mainAxisAlignment: MainAxisAlignment.spaceBetween,
               children: [
-                CachedNetworkImage(imageUrl: brandImage,width: 75,height: 75),
+                ClipRRect(
+                  borderRadius: BorderRadius.circular(50),
+                  child: CachedNetworkImage(
+                    imageUrl: brandImage,
+                    width: 55,
+                    height: 55,
+                    fit: BoxFit.cover,
+                  ),
+                ),
                 Column(
                   crossAxisAlignment: CrossAxisAlignment.start,
                   children: [
-                    Text(jobTitle,style: TextStyle(fontSize: 22, fontWeight: FontWeight.w600,color: MyColors.white),),
-                    Text(companyName,style: TextStyle(color: MyColors.C_E9E9EFF),),
+                    Text(
+                      jobTitle,
+                      style: TextStyle(
+                          fontSize: 22,
+                          fontWeight: FontWeight.w600,
+                          color: MyColors.white),
+                    ),
+                    Text(
+                      companyName,
+                      style: TextStyle(color: MyColors.C_E9E9EFF),
+                    ),
                   ],
                 ),
-                SvgPicture.asset(MyIcons.status, width: 25,)
-
-
+                SvgPicture.asset(
+                  MyIcons.status,
+                  width: 25,
+                )
               ],
             ),
             Row(
@@ -78,28 +93,28 @@ class VacancyItem extends StatelessWidget {
                   width: 20,
                   decoration: BoxDecoration(
                       color: MyColors.C_E9E9EFF,
-                      borderRadius: BorderRadius.circular(8)
-                  ),
-                  child: Center(child: Text(jobType!=0?"IT": "Desigin")),
+                      borderRadius: BorderRadius.circular(8)),
+                  child: Center(child: Text(jobType != 0 ? "IT" : "Desigin")),
                 ),
-
                 Container(
                   height: 25,
                   padding: const EdgeInsets.symmetric(horizontal: 10),
                   decoration: BoxDecoration(
                       color: MyColors.C_E9E9EFF,
-                      borderRadius: BorderRadius.circular(8)
-                  ),
-                  child: Center(child: Text(fromWhere == 0 ? "Full-Time" :
-                  fromWhere == 1 ? "Part-Time" : "any")),
+                      borderRadius: BorderRadius.circular(8)),
+                  child: Center(
+                      child: Text(fromWhere == 0
+                          ? "Full-Time"
+                          : fromWhere == 1
+                              ? "Part-Time"
+                              : "any")),
                 ),
                 Container(
-                   height: 25,
+                  height: 25,
                   padding: const EdgeInsets.symmetric(horizontal: 10),
                   decoration: BoxDecoration(
-                    color: MyColors.C_E9E9EFF,
-                    borderRadius: BorderRadius.circular(8)
-                  ),
+                      color: MyColors.C_E9E9EFF,
+                      borderRadius: BorderRadius.circular(8)),
                   child: Center(child: Text(requiredLevel)),
                 )
               ],
@@ -107,17 +122,20 @@ class VacancyItem extends StatelessWidget {
             Row(
               mainAxisAlignment: MainAxisAlignment.spaceBetween,
               children: [
-                Text(offeredSalary, style: TextStyle(color: MyColors.white,fontWeight: FontWeight.w600),),
-                Text(recruiterPhone, style: TextStyle(color: MyColors.white),)
+                Text(
+                  offeredSalary,
+                  style: TextStyle(
+                      color: MyColors.white, fontWeight: FontWeight.w600),
+                ),
+                Text(
+                  recruiterPhone,
+                  style: TextStyle(color: MyColors.white),
+                )
               ],
             )
-
-
           ],
         ),
       ),
     );
   }
 }
-
-
